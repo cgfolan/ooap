be7ec6638534dd4a90f93520d54b320644443f1c
cgfolan <cgfolan@gmail.com>
2021-03-27 23:14:05 +0000

    Begin board printing code(ugly)

---
+++ XOboard.py
@@ -0,0 +1,18 @@
+A1 = ['']
+A2 = ['']
+A3 = ['']
+B1 = ['']
+B2 = ['']
+B3 = ['']
+C1 = ['']
+C2 = ['']
+C3 = ['']
+
+Row0 = ['        1 |   2  |  3 ']
+Row1 = 'A |',A1,A2,A3
+Row2 = ['B |',B1,B2,B3,]
+Row3 = ['C |',C1,C2,C3,]
+print(Row0)
+print(Row1)
+print(Row2)
+print(Row3)
