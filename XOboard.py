d572574806a05ed00003ecfd8758adbd3974ccf7
cgfolan <cgfolan@gmail.com>
2021-03-27 23:37:29 +0000

    Developed, and added comment

--- XOboard.py
+++ XOboard.py
@@ -1,18 +1,21 @@
-A1 = ['']
-A2 = ['']
-A3 = ['']
-B1 = ['']
-B2 = ['']
-B3 = ['']
-C1 = ['']
-C2 = ['']
-C3 = ['']
+A1 = 'X'
+A2 = 'O'
+A3 = 'X'
+B1 = 'X'
+B2 = 'X'
+B3 = 'X'
+C1 = 'O'
+C2 = 'O'
+C3 = 'O'
 
-Row0 = ['        1 |   2  |  3 ']
-Row1 = 'A |',A1,A2,A3
-Row2 = ['B |',B1,B2,B3,]
-Row3 = ['C |',C1,C2,C3,]
+#Creating test for board printing using individual variables for each space on the board
+
+Row0 = '       1    2    3 '
+Row1 = 'A',A1,A2,A3
+Row2 = 'B',B1,B2,B3,
+Row3 = 'C',C1,C2,C3,
 print(Row0)
 print(Row1)
 print(Row2)
 print(Row3)
+
