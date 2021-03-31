ede1d1c3494b7c6e3f835e179d58d33cc5d7e026
cgfolan <cgfolan@gmail.com>
2021-03-31 19:31:57 +0000

    Created graph for board in test file

--- XOboard.py
+++ XOboard.py
@@ -19,3 +19,18 @@ print(Row1)
 print(Row2)
 print(Row3)
 
+
+import math
+SQ = math.sqrt(11)
+board_graph = {
+    'A1' : {'A2':1,'B1':10, 'B2': SQ},
+    'A2' : {'A1':1, 'A3':1,'B1': SQ,'B2':10,'B3':SQ},
+    'A3' : {'A2':1, 'B2':SQ, 'B3':10},
+    'B1' : {'A1':10,'A2':SQ,'B2':1,'C1':10,'C2':SQ},
+    'B2' : {'A1':SQ,'A2':10,'A3':SQ,'B1':1,'B3':1,'C1':SQ,'C2':10,'C3':SQ},
+    'B3' : {'A2':SQ,'B2':1,'C2':SQ,'C3':10},
+    'C1' : {'B1':5,'B2':SQ,},
+    'C2' : {'C1':1,'C3':1,'B1':SQ,'B2':10,'B3':SQ}
+}
+
+print(SQ + SQ)
