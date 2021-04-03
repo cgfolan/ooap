6f43c02728670eec8db384b82a83f84025d7a23a
cgfolan <cgfolan@gmail.com>
2021-04-03 19:30:16 +0000

    Improved board viz, finished player loop

--- MainXO.py
+++ MainXO.py
@@ -1,36 +1,38 @@
 import math
 import random
+SQ = math.sqrt(11)
+board_graph: 
+    'A1' : {'A2':1,'B1':10, 'B2': SQ},
+    'A2' : {'A1':1, 'A3':1,'B1': SQ,'B2':10,'B3':SQ},
+    'A3' : {'A2':1, 'B2':SQ, 'B3':10},
+    'B1' : {'A1':10,'A2':SQ,'B2':1,'C1':10,'C2':SQ},
+    'B2' : {'A1':SQ,'A2':10,'A3':SQ,'B1':1,'B3':1,'C1':SQ,'C2':10,'C3':SQ},
+    'B3' : {'A2':SQ,'B2':1,'C2':SQ,'C3':10},
+    'C1' : {'B1':10,'B2':SQ},
+    'C2' : {'C1':1,'C3':1,'B1':SQ,'B2':10,'B3':SQ}
 
 
-A1 = ''
-A2 = ''
-A3 = ''
-B1 = ''
-B2 = ''
-B3 = ''
-C1 = ''
-C2 = ''
-C3 = ''
 
+board_list = [[' '],[' '],[' '],[' '],[' '],[' '],[' '],[' '],[' ']]
 
-Row0 = '       1    2    3 '
-Row1 = 'A',A1,A2,A3
-Row2 = 'B',B1,B2,B3
-Row3 = 'C',C1,C2,C3  
+def printboard(): 
+    print('_________________________'),
+    print('|       |       |       |'),
+    print('|',board_list[0],'|',board_list[1],'|',board_list[2],'|'),
+    print('|       |       |       |'),
+    print('|-----------------------|'),
+    print('|       |       |       |'),
+    print('|',board_list[3],'|',board_list[4],'|',board_list[5],'|'),
+    print('|       |       |       |'),
+    print('|-----------------------|'),
+    print('|       |       |       |'),
+    print('|',board_list[6],'|',board_list[7],'|',board_list[8],'|'),
+    print('|       |       |       |'),
+    print('|-----------------------|')
 
-def printboard():
-    print(Row0),
-    print(Row1),
-    print(Row2),
-    print(Row3)    
-
+last_move = 'first'
 occupied_squares = []
-
 play_again = 'y'
-
-printboard()
-
-
 
 while play_again == 'y' or 'Yes':
     #first player chosen at random
@@ -41,7 +43,7 @@ while play_again == 'y' or 'Yes':
     else:
         print('Computer to go first.')
         player_counter = 'O'   
-
+    printboard()
 # Ask for player input and check for input errors
     while player_counter == 'X':    
         # Use try block to evaluate for errors
@@ -58,8 +60,25 @@ while play_again == 'y' or 'Yes':
         # Check if square is occupied
         if player_move in occupied_squares:
             print('Square already occupied! Try Again.')
+        player_move -= 1
         occupied_squares.append(player_move)
+        board_list.pop(player_move)
+        board_list.insert(player_move,'  X  ')
+        # last_move = player_move
+        printboard()
+        player_counter = 'O'
+
+    while player_counter == 'O':
+        if last_move == 'first':
+            comp_move = random.randint(0,8)
+            occupied_squares.append(comp_move)
+            board_list.pop(comp_move)
+            board_list.insert(comp_move,'  O  ')
+            printboard()
+            player_counter = 'X'
         
+
+
 
         
 
--- XOboard_practice.py
+++ XOboard_practice.py
@@ -1,37 +1,56 @@
-A1 = 'X'
-A2 = 'O'
-A3 = 'X'
-B1 = 'X'
-B2 = 'X'
-B3 = 'X'
-C1 = 'O'
-C2 = 'O'
-C3 = 'O'
+# A1 = 'X'
+# A2 = 'O'
+# A3 = 'X'
+# B1 = 'X'
+# B2 = 'X'
+# B3 = 'X'
+# C1 = 'O'
+# C2 = 'O'
+# C3 = 'O'
 
-#Creating test for board printing using individual variables for each space on the board
+# #Creating test for board printing using individual variables for each space on the board
 
-Row0 = '       1    2    3 '
-Row1 = 'A',A1,A2,A3
-Row2 = 'B',B1,B2,B3,
-Row3 = 'C',C1,C2,C3,
-print(Row0)
-print(Row1)
-print(Row2)
-print(Row3)
+# Row0 = '       1    2    3 '
+# Row1 = 'A',A1,A2,A3
+# Row2 = 'B',B1,B2,B3,
+# Row3 = 'C',C1,C2,C3,
+# print(Row0)
+# print(Row1)
+# print(Row2)
+# print(Row3)
 
 #graph of the nodes, with horizontal and verticals
-#to determine win conditions
-import math
-SQ = math.sqrt(11)
-board_graph = {
-    'A1' : {'A2':1,'B1':10, 'B2': SQ},
-    'A2' : {'A1':1, 'A3':1,'B1': SQ,'B2':10,'B3':SQ},
-    'A3' : {'A2':1, 'B2':SQ, 'B3':10},
-    'B1' : {'A1':10,'A2':SQ,'B2':1,'C1':10,'C2':SQ},
-    'B2' : {'A1':SQ,'A2':10,'A3':SQ,'B1':1,'B3':1,'C1':SQ,'C2':10,'C3':SQ},
-    'B3' : {'A2':SQ,'B2':1,'C2':SQ,'C3':10},
-    'C1' : {'B1':10,'B2':SQ,},
-    'C2' : {'C1':1,'C3':1,'B1':SQ,'B2':10,'B3':SQ}
-}
+# #to determine win conditions
+# import math
+# SQ = math.sqrt(11)
+# board_graph = {
+#     'A1' : {'A2':1,'B1':10, 'B2': SQ},
+#     'A2' : {'A1':1, 'A3':1,'B1': SQ,'B2':10,'B3':SQ},
+#     'A3' : {'A2':1, 'B2':SQ, 'B3':10},
+#     'B1' : {'A1':10,'A2':SQ,'B2':1,'C1':10,'C2':SQ},
+#     'B2' : {'A1':SQ,'A2':10,'A3':SQ,'B1':1,'B3':1,'C1':SQ,'C2':10,'C3':SQ},
+#     'B3' : {'A2':SQ,'B2':1,'C2':SQ,'C3':10},
+#     'C1' : {'B1':10,'B2':SQ,},
+#     'C2' : {'C1':1,'C3':1,'B1':SQ,'B2':10,'B3':SQ}
+# }
 
-print(SQ + SQ)
+# print(SQ + SQ)
+
+board_list = [[' '],['o'],['o'],['o'],['x'],['o'],['x'],['o'],['x']]
+def printboard(): 
+    print('_________________________'),
+    print('|       |       |       |'),
+    print('|',board_list[0],'|',board_list[1],'|',board_list[2],'|'),
+    print('|       |       |       |'),
+    print('|-----------------------|'),
+    print('|       |       |       |'),
+    print('|',board_list[3],'|',board_list[4],'|',board_list[5],'|'),
+    print('|       |       |       |'),
+    print('|-----------------------|'),
+    print('|       |       |       |'),
+    print('|',board_list[6],'|',board_list[7],'|',board_list[8],'|'),
+    print('|       |       |       |'),
+    print('|-----------------------|')
+
+
+printboard()
