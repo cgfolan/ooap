191b588016d2a93fe02566929bf387059af3b07f
cgfolan <cgfolan@gmail.com>
2021-04-03 13:47:17 +0000

    Set up logic checks

--- MainXO.py
+++ MainXO.py
@@ -1,6 +1,7 @@
 import math
 import random
-#resets the board
+
+
 A1 = ''
 A2 = ''
 A3 = ''
@@ -11,12 +12,11 @@ C1 = ''
 C2 = ''
 C3 = ''
 
+
 Row0 = '       1    2    3 '
 Row1 = 'A',A1,A2,A3
 Row2 = 'B',B1,B2,B3
 Row3 = 'C',C1,C2,C3  
-
-
 
 def printboard():
     print(Row0),
@@ -24,24 +24,48 @@ def printboard():
     print(Row2),
     print(Row3)    
 
+occupied_squares = []
+
 play_again = 'y'
+
+printboard()
+
+
 
 while play_again == 'y' or 'Yes':
     #first player chosen at random
     coin_flip = random.randint(0,100)
     if (coin_flip % 2 == 0):
         print('You go first!')
-        player_counter = 1
+        player_counter = 'X'
     else:
         print('Computer to go first.')
-        player_counter = 0     
+        player_counter = 'O'   
 
-    while player_counter == 1:
-        player_move = (input('Pick a number 1-9: '))
-        if player_move != '1' or '2' or '3' or '4' or '5' or '6' or '7' or '8' or '9':
-            print('Input must be 0-9')
-            
+# Ask for player input and check for input errors
+    while player_counter == 'X':    
+        # Use try block to evaluate for errors
+        try:
+            player_move =int(input('Pick a box 1-9: '))
+            # Check in input in not an int
+        except ValueError:
+            print('Please pick a number 1-9')
+            continue
+        # Check if input is within the range 1-9
+        if player_move < 0 or player_move > 9:
+            print('Illegal move! Try again.')
+            continue
+        # Check if square is occupied
+        if player_move in occupied_squares:
+            print('Square already occupied! Try Again.')
+        occupied_squares.append(player_move)
         
+
+        
+
+
+        
+
 
 
   
